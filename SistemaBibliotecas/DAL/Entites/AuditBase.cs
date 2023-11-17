82a59e4a72dae6affc3b337c3b24e7b74be24f53
Alexis <oschan2004@gmail.com>
2023-11-17 15:38:01 -0500

    AuditBase created

---
+++ SistemaBibliotecas/DAL/Entites/AuditBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaBibliotecas.DAL.Entites
+{
+    public class AuditBase
+    {
+        [Key]
+        [Required]
+
+        public virtual Guid Id { get; set; }
+    }
+}
